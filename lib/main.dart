4455cbc586a857ba17dc4c6a14a3c5957d8aaaad
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-19 22:15:18 +0700

    Change back to showing splash screen upon entering app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:setor_mobil/screens/page/home_screen.dart';
+import 'package:setor_mobil/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -108,7 +108,8 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
 
-      home: const HomeScreen(),
+      // Changed from HomeScreen to SplashScreen
+      home: const SplashScreen(),
     );
   }
 }
